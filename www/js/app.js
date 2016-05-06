5efd9d560e554cc0e25e717f7ea984e2b3857f69
Vincent Fiestada <vffiestada@up.edu.ph>
2016-05-06 21:40:45 +0800

    Display 10 latest readings from sensor

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,12 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'chart.js'])
+angular.module('starter', ['ionic', 'ngMaterial', 'chart.js', 'angularMoment'])
+
+.config(function($mdIconProvider)
+{
+  $mdIconProvider.defaultIconSet("svg/mdi.svg");
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,18 +28,67 @@ angular.module('starter', ['ionic', 'chart.js'])
   });
 })
 
-.controller("WaterHistoryController", function($scope) {
-    $scope.labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    $scope.series = ['Water Level'];
-    $scope.data = [
-        [65, 59, 80, 81, 56, 55, 60],
-    ];
-})
-
-.controller("DayHistoryController", function($scope) {
-    $scope.labels = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    $scope.series = ['Days Without Replacement'];
-    $scope.data = [
-        [0, 100, 200, 0, 100, 200, 300],
-    ];
+.controller("WaterHistoryController", function($scope, $http, moment, $timeout)
+{
+  // DeviceID and Config are hardcoded for now
+  $scope.deviceId = "571d8bd1e9786ce82788bed2";
+	var config = 
+  {
+    "headers":
+    {
+      "x-access-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6InNhYmVyIiwiaWQiOiI1NzFkNzg2NGY4MWQ4ZjNjMWZhZTFkYmEiLCJpYXQiOjE0NjI1MzE1NzUsImV4cCI6MTQ2MzEzNjM3NSwiaXNzIjoicG9zZWlkb24ifQ.2ldkJGcDOFnRJ9zvP1BIYY4xK0WeTB5QdYeEWhHVbCs"
+    }
+  };
+	var updateTask = null;
+  $scope.series = ["Sensor Readings"];
+	$scope.getDevice = function()
+	{
+		$http.get("/api/device/" + $scope.deviceId + "?t=" + Date.now().toString(), config)
+		.then(function success(res)
+		{
+			if (updateTask)
+			{
+				$timeout.cancel(updateTask);
+				updateTask = null;
+			}
+			$scope.device = res.data;
+			// Get sensor data
+			$scope.levels = [[]];
+			$scope.labels = [];
+      if (res.data.readings.length > 10)
+      {
+        res.data.readings.splice(0, res.data.readings.length - 10);
+      }
+			for(var i = 0; i < res.data.readings.length; i++)
+			{
+				$scope.levels[0].push(res.data.readings[i].level);
+				$scope.labels.push(moment(res.data.readings[i].dateSent).fromNow());
+			}
+			if ($scope.levels[0].length > 0)
+      {
+        // Check if stagnant and/or full
+        $scope.isStagnant = false;
+        $scope.isFull = false;
+        var now = new Date();
+        var lastEmptied = new Date(res.data.lastEmptied);
+        var diff = now.getTime() - lastEmptied.getTime();
+        if (diff >= 259200000) // 259200000 ms is 3 days
+        {
+          $scope.isStagnant = true;
+        }
+        $scope.percentage = 100 * $scope.levels[0][+$scope.levels[0].length - 1] / $scope.device.maxCapacity;
+        if ($scope.percentage >= 90)
+        {
+          $scope.isFull = true;
+        }
+      }
+			// Set timeout for next update 
+			var updateTask = $timeout($scope.getDevice, 4000);
+		},
+		function error(res)
+		{
+			// DO something on error
+		});
+	};
+	$scope.getDevice(); // Get devices for the first time
 });
